3fc04b643e4aaf12d3c97ec4cc70142f9c8fa54e
Disha Thummala <disha.thummala@gmail.com>
2025-06-26 16:18:23 +0530

    update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "embonics@syslabs"
 
 def init_db():
-    conn = sqlite3.connect(customers.db)
+    conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     c.execute('''
         CREATE TABLE IF NOT EXISTS customers (
